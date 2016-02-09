85a39ef42f0b2540ec8b40798ebf308651629d08
Sam Foster <sfoster@mozilla.com>
2016-02-09 00:44:37 -0800

    (re)start.sh, run button service as 'pi'

--- start.sh
+++ start.sh
@@ -2,11 +2,16 @@
 
 cd `dirname $0`
 
+source common/defaults
+
 # start up mosquitto
+sudo /etc/init.d/mosquitto stop
 mosquitto --daemon
 
 # start up our button publisher
-/etc/init.d/buttonservice.sh start
+sudo /etc/init.d/buttonservice.sh stop
+sudo /etc/init.d/buttonservice.sh start
 
 # start up the mqtt monitor
+pm2 stop monitor/index.js && pm2 delete monitor/index.js
 pm2 start monitor/index.js
