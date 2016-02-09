6ca9e8b63a71700c86be1b1cfc79b0ab24dc335e
Sam Foster <sfoster@mozilla.com>
2016-02-09 01:23:12 -0800

    Make a config.json for monitor

--- start.sh
+++ start.sh
@@ -13,5 +13,6 @@ sudo /etc/init.d/buttonservice.sh stop
 sudo /etc/init.d/buttonservice.sh start
 
 # start up the mqtt monitor
-pm2 stop monitor/index.js && pm2 delete monitor/index.js
-pm2 start monitor/index.js
+pm2 stop monitor && pm2 delete monitor
+sed "s|__MQTT_HOST__|$MQTT_HOST|" monitor/config.template > monitor/config.json
+pm2 start monitor/config.json --name="monitor"
