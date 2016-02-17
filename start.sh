71db00c71a030724b7dea2f7a472aeeed40d4903
Sam Foster <sfoster@mozilla.com>
2016-02-17 02:20:20 +0000

    Add some logging to start.sh

--- start.sh
+++ start.sh
@@ -5,22 +5,30 @@ command_exists () {
 }
 
 cd `dirname $0`
+logfile=logs/start.log
+echo running start.sh at `date "+%c"` > $logfile
 
 [ -f ./common/defaults ] && . common/defaults
-echo "MQTT_HOST: $MQTT_HOST"
+echo "MQTT_HOST: $MQTT_HOST" >> $logfile
 
 # start up mosquitto
 # start up our button publisher
 if command_exists mosquitto ; then
    sudo /etc/init.d/mosquitto stop
    mosquitto --daemon
+   echo "Started mosquitto" >> $logfile
+else
+   echo "Couldnt start mosquitto" >> $logfile
 fi
 
 # start up our button publisher
 MQTT_HOST=$MQTT_HOST ./buttonpi/button.py &
+echo "Started button publisher" >> $logfile
 
 # start up the node apps
 pm2 stop all
 sed "s|__MQTT_HOST__|$MQTT_HOST|" ./config.template > ./config.json
 pm2 start ./config.json
+echo "Started apps with pm2" >> $logfile
 
+echo "Done" >> $logfile
